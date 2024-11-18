397ce9c6c53bb67bb3ed02566aa55892e62de42a
Kekeli Afantchao <kelkiiii@mithrandir.local>
2024-11-18 15:49:37 -0500

    comments

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import "dotenv/config";
 import express, { Request, Response } from "express";
-import axios, {AxiosResponse} from "axios";
+import axios, { AxiosResponse } from "axios";
 import dotenv from "dotenv";
-import { Movie, DiscoverMoviesResponse, CrewMember, Editors, HomerunMovie } from "./types.js";
+import {
+  Movie,
+  DiscoverMoviesResponse,
+  CrewMember,
+  Editors,
+  HomerunMovie,
+} from "./types.js";
 
+// initialiaze express server
 const app = express();
 const PORT = process.env.PORT || 3000;
 dotenv.config();
@@ -13,18 +20,18 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
+// ensuring server is able to reach base root
 app.get("/", (req: Request, res: Response) => {
-  res.send({message: "Server is up and running!"})
-}) 
+  res.send({ message: "Server is up and running!" });
+});
 
-
+// main logic to gather movie and credits data
 app.get("/movies/:year", async (req: Request, res: Response) => {
   const { year } = req.params;
   // Interface Extends this & this for homerun response
-  
-  const homerun: any = []
+
+  // final variable which will be sent out as a response to client
+  const homerun: any = [];
 
   try {
     const response: AxiosResponse<DiscoverMoviesResponse> = await axios.get(
@@ -34,17 +41,17 @@ app.get("/movies/:year", async (req: Request, res: Response) => {
           Authorization: process.env.BEARER,
         },
       }
-    )
+    );
 
     const movies: Movie[] = response.data.results;
-    
+
     for (const movie of movies) {
       homerun.push({
         title: movie.title,
         release_date: movie.release_date,
         vote_average: movie.vote_average,
         editors: [],
-      })
+      });
     }
 
     const credit_promises = response.data.results.map((movie: any, i: any) => {
@@ -55,36 +62,36 @@ app.get("/movies/:year", async (req: Request, res: Response) => {
           },
         })
         .then((credits_response) => {
-          const editors: Editors = credits_response.data.crew.filter(
-            (crew_member: CrewMember) => crew_member.known_for_department === "Editing"
-          ).map((editor: Editors) => editor.name);
+          const editors: Editors = credits_response.data.crew
+            .filter(
+              (crew_member: CrewMember) =>
+                crew_member.known_for_department === "Editing"
+            )
+            .map((editor: Editors) => editor.name);
           console.log(editors);
-          
+
           homerun[i] = {
             ...homerun[i],
             editors,
-          }
+          };
         });
-      });
-      
+    });
+
+    // waiting for all promises from credit_promises ensures the homerun variable will not be sent to client without editors
     await Promise.all(credit_promises);
     res.status(200).send(homerun as HomerunMovie[]);
     return homerun;
-    // make an interface for the responses
-    // Partial<InterfaceName>
-
   } catch (error) {
     console.error("error fetching movies", error);
 
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
     } else {
-      res.status(500).json({ message: "An unknown error occured"})
+      res.status(500).json({ message: "An unknown error occured" });
     }
   }
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Welcome to express, listening on https://localhost:${PORT}`)
+  console.log(`Welcome to express, listening on https://localhost:${PORT}`);
 });
